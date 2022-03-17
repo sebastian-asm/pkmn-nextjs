f8110c87f7ddde8afb04be8c5d95d2156c4ec0ab
Sebastian Sanchez <sebastian.asm@outlook.com>
2022-03-17 15:16:03 -0300

    agregadas meta para open graph

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -7,13 +7,25 @@ interface Props {
   title?: string;
 }
 
+// comprobar cuando este el object window disponible
+const origin = typeof window !== 'undefined' ? window.location.origin : '';
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
       <Head>
         <title>{title || 'PKMN Web'}</title>
         <meta name="author" content="Sebastián" />
-        <meta name="description" content="Información del Pokémon..." />
+        <meta name="description" content={`Información del Pokémon ${title}`} />
+
+        {/* open graph meta tags */}
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content={`Información del Pokémon ${title}`}
+        />
+        {/* se recomineda que la url de la imagen sea su ruta absoluta */}
+        <meta property="og:image" content={`${origin}/assets/img/banner.png`} />
       </Head>
       <Navbar />
       <main style={{ padding: '0 20px' }}>{children}</main>
